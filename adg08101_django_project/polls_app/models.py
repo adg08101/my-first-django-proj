1e05b1714318dcfeb683aef36d15555324e125c5
klol <adg08101@gmail.com>
2021-04-21 16:44:03 -0400

    Initial commit
    Initial commit

--- adg08101_django_project/polls_app/models.py
+++ adg08101_django_project/polls_app/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return f"The question id is {self.id} with {self.question_text} text and added on {self.pub_date}"
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -12,6 +20,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 
-class Notes(models.Model):
+class Note(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     note = models.CharField(max_length=150)
     pub_date = models.DateTimeField('date published')
