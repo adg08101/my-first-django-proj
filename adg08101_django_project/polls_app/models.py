8a0774f3ec52f5265e6af4b41aa825bf6e9143ba
klol <adg08101@gmail.com>
2021-04-21 12:25:47 -0400

    Initial commit
    Initial commit

--- adg08101_django_project/polls_app/models.py
+++ adg08101_django_project/polls_app/models.py
@@ -10,3 +10,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Notes(models.Model):
+    note = models.CharField(max_length=150)
+    pub_date = models.DateTimeField('date published')
