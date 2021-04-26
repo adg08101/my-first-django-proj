e4e85d552c27bcf579e41e0013d71fb1adecb564
klol <adg08101@gmail.com>
2021-04-26 14:57:55 -0400

    Initial commit

--- adg08101_django_project/poll_app/models.py
+++ adg08101_django_project/poll_app/models.py
@@ -19,23 +19,23 @@ class QuestionType(models.Model):
         return f"{self.type_text}"
 
 
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text}"
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date published')
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     type = models.ManyToManyField(QuestionType)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.question_text} " + datetime.datetime.strftime(self.pub_date, '%b %d, %Y')
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.choice_text}"
